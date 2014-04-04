57092c38bf00600326569b58edb88ec57c2d1514
Scott Clasen <scott@heroku.com>
2014-04-03 19:22:04 -0700

    remove pipework

--- start-kafka.sh
+++ start-kafka.sh
@@ -6,6 +6,4 @@ echo "zookeeper.connect=$ZK_CONN" >> /kafka/conf/server.properties
 echo "host.name=$HOSTNAME" >> /kafka/conf/server.properties
 echo "broker.id=1" >> /kafka/conf/server.properties
 
-/pipework --wait
-
 /kafka/kafka_2.9.2-0.8.1/bin/kafka-server-start.sh /kafka/conf/server.properties
